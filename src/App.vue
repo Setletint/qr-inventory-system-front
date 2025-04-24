17c058f3e945156691c6d0e0a30ccc4cf99d1ba4
Setletint <72040591+Setletint@users.noreply.github.com>
2025-04-24 22:23:10 +0300

    added daisyui to css

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <button class="btn btn-primary">Button</button>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
