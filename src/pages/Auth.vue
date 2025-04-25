41b7f80a4123eddc2973e26643f328444cbf2ba2
Setletint <72040591+Setletint@users.noreply.github.com>
2025-04-25 16:11:59 +0300

    Created RegisterForm

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,18 +1,20 @@
 <template>
-    <div class="min-h-screen flex flex-col items-center justify-center">
-      <div class="tabs mb-4">
-        <button class="tab tab-bordered" :class="{ 'tab-active': tab === 'login' }" @click="tab = 'login'">Login</button>
-        <button class="tab tab-bordered" :class="{ 'tab-active': tab === 'register' }" @click="tab = 'register'">Register</button>
-      </div>
-      <LoginForm v-if="tab === 'login'" />
-      <!--<RegisterForm v-else /> -->
+  <div class="min-h-screen flex flex-col items-center justify-center">
+    <div class="tabs mb-4">
+      <button class="tab tab-bordered" :class="{ 'tab-active': tab === 'login' }" @click="tab = 'login'">Login</button>
+      <button class="tab tab-bordered" :class="{ 'tab-active': tab === 'register' }"
+        @click="tab = 'register'">Register</button>
     </div>
-  </template>
-  
-  <script lang="ts" setup>
-  import { ref } from 'vue';
-  import LoginForm from '../components/auth/LoginForm.vue';
-  // Import Login and Register form
-  
-  const tab = ref<'login' | 'register'>('login');
-  </script>
+    <LoginForm v-if="tab === 'login'" />
+    <RegisterForm v-else />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import LoginForm from '../components/auth/LoginForm.vue';
+import RegisterForm from '../components/auth/RegisterForm.vue';
+
+
+const tab = ref<'login' | 'register'>('login');
+</script>
