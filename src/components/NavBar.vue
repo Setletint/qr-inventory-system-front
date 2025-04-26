f5455446bc561b91310d4acc6291b697a9d78ba8
Setletint <72040591+Setletint@users.noreply.github.com>
2025-04-26 20:55:58 +0300

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,12 +14,6 @@
 
         <!-- Right side -->
         <div class="ml-auto flex-none">
-
-            <div v-if="isAuthenticated" tabindex="0" role="button" class="btn btn-ghost btn-circle avatar mr-2">
-                <div class="w-10 rounded-full">
-                    <span class="text-xl text-green-500 leading-9">+</span>
-                </div>
-            </div>
             <div v-if="isAuthenticated" class="dropdown dropdown-end">
                 <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
                     <div class="w-10 rounded-full">
@@ -27,12 +21,12 @@
                     </div>
                 </div>
                 <ul tabindex="0"
-                    class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                    class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-64">
                     <li>
-                        <router-link to="/settings">Settings</router-link>
+                        <router-link class="text-2xl md:text-lg" to="/settings">Settings</router-link>
                     </li>
                     <li>
-                        <button @click="logout">Logout</button>
+                        <button class="text-2xl md:text-lg" @click="logout">Logout</button>
                     </li>
                 </ul>
             </div>
